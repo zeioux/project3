0655927b4c9727650ff3a41d7c940908038ef77c
annoying proj <torvicetlinece@gmail.com>
2025-04-04 23:23:54 +0200

    dzq

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/project3/',  // Remplace "project3" par le nom de ton repository
+  base: './',  // Remplace "project3" par le nom de ton repository
   plugins: [react()],
 });
